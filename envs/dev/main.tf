fec03a03c9dcfb717847b3680f1ab1dce027e7ed
hikobend <hiko.bkend@gmail.com>
2023-08-02 20:00:17 +0900

    S3とDyanmoDBを作成

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,3 +1,40 @@
-resource "aws_vpc" "name" {
-  cidr_block = "10.0.0.0/20"
+module "s3_bucket_dev_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "dev-bucket-architecture-s3-bucket-ecs"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    name = "dev-bucket-architecture-s3-bucket-ecs"
+  }
+}
+
+module "dynamodb_table" {
+  source                         = "terraform-aws-modules/dynamodb-table/aws"
+  name                           = "set-dev-dynamo-db-ecs"
+  billing_mode                   = "PAY_PER_REQUEST"
+  hash_key                       = "LockID"
+  server_side_encryption_enabled = true
+  point_in_time_recovery_enabled = true
+
+  attributes = [
+    {
+      name = "LockID"
+      type = "S"
+    }
+  ]
 }
