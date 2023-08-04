0c1ec61561ac32a88cc72b505e25d5681c46e820
hikobend <hiko.bkend@gmail.com>
2023-08-04 09:56:26 +0900

    network作成

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "4.0.0"
 
-  vpc_cidr               = "10.0.0.0/24"
+  cidr                   = "10.0.0.0/24"
   azs                    = ["ap-northeast-1a", "ap-northeast-1c"]
   public_subnets         = ["10.0.0.0/26", "10.0.0.64/26"]
   private_subnets        = ["10.0.0.128/26", "10.0.0.192/26"]
