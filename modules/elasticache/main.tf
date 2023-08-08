200c3fa491ffb9175385f792e7f7607165da0608
hikobend <hiko.bkend@gmail.com>
2023-08-08 09:46:57 +0900

    oidc修正

--- modules/elasticache/main.tf
+++ modules/elasticache/main.tf
@@ -6,10 +6,6 @@ resource "aws_cloudwatch_log_group" "engine_log" {
   name = "${var.env}-redis-cluster-engine-log"
 }
 
-resource "aws_iam_service_linked_role" "this" {
-  aws_service_name = "elasticache.amazonaws.com"
-}
-
 resource "aws_elasticache_subnet_group" "this" {
   name       = "${var.env}-redis-subnet-group"
   subnet_ids = [var.private_subnet_1a, var.private_subnet_1c]
@@ -18,10 +14,10 @@ resource "aws_elasticache_subnet_group" "this" {
 resource "aws_elasticache_cluster" "this" {
   cluster_id         = "${var.env}-redis-cluster"
   engine             = "redis"
-  node_type          = "cache.t3.micro"
-  num_cache_nodes    = 1
+  node_type          = var.redis_node_type
+  num_cache_nodes    = var.redis_num_cache_nodes
   port               = 6379
-  engine_version     = "6.2"
+  engine_version     = var.redis_engine_version
   apply_immediately  = true
   subnet_group_name  = aws_elasticache_subnet_group.this.name
   availability_zone  = var.availability_zone_1a
