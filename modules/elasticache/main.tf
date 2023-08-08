a24bd129b7d350adcc885cbe77ec39d19aeee58c
hikobend <hiko.bkend@gmail.com>
2023-08-08 21:37:27 +0900

    elasticahceの保持期間を10日にする

--- modules/elasticache/main.tf
+++ modules/elasticache/main.tf
@@ -22,16 +22,17 @@ resource "aws_elasticache_subnet_group" "this" {
 }
 
 resource "aws_elasticache_cluster" "this" {
-  cluster_id         = "${var.env}-redis-cluster"
-  engine             = "redis"
-  node_type          = var.node_type
-  num_cache_nodes    = var.num_cache_nodes
-  port               = 6379
-  engine_version     = var.engine_version
-  apply_immediately  = true
-  subnet_group_name  = aws_elasticache_subnet_group.this.name
-  availability_zone  = var.availability_zone_1a
-  security_group_ids = [var.redis_sg_id]
+  cluster_id               = "${var.env}-redis-cluster"
+  engine                   = "redis"
+  node_type                = var.node_type
+  num_cache_nodes          = var.num_cache_nodes
+  port                     = 6379
+  engine_version           = var.engine_version
+  apply_immediately        = true
+  snapshot_retention_limit = 10
+  subnet_group_name        = aws_elasticache_subnet_group.this.name
+  availability_zone        = var.availability_zone_1a
+  security_group_ids       = [var.redis_sg_id]
   log_delivery_configuration {
     destination      = aws_cloudwatch_log_group.slow_log.name
     destination_type = "cloudwatch-logs"
