fa83b1378eed66a1f1c5093fa7361279054fd7cb
hikobend <hiko.bkend@gmail.com>
2023-08-08 09:41:12 +0900

    oidc修正

--- modules/elasticache/main.tf
+++ modules/elasticache/main.tf
@@ -22,7 +22,7 @@ resource "aws_elasticache_cluster" "this" {
   num_cache_nodes    = 1
   port               = 6379
   engine_version     = "6.2"
-  apply_immediately  = false
+  apply_immediately  = true
   subnet_group_name  = aws_elasticache_subnet_group.this.name
   availability_zone  = var.availability_zone_1a
   security_group_ids = [var.redis_sg_id]
