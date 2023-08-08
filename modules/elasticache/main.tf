b85b3a77b0528b78c9294ccf12ba030ebe5ce09f
hikobend <hiko.bkend@gmail.com>
2023-08-08 09:48:39 +0900

    redisをlocalから設定

--- modules/elasticache/main.tf
+++ modules/elasticache/main.tf
@@ -14,10 +14,10 @@ resource "aws_elasticache_subnet_group" "this" {
 resource "aws_elasticache_cluster" "this" {
   cluster_id         = "${var.env}-redis-cluster"
   engine             = "redis"
-  node_type          = var.redis_node_type
-  num_cache_nodes    = var.redis_num_cache_nodes
+  node_type          = var.node_type
+  num_cache_nodes    = var.num_cache_nodes
   port               = 6379
-  engine_version     = var.redis_engine_version
+  engine_version     = var.engine_version
   apply_immediately  = true
   subnet_group_name  = aws_elasticache_subnet_group.this.name
   availability_zone  = var.availability_zone_1a
