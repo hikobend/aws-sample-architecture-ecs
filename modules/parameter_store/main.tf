ce8e3ff3c0c2825649f484ea9f93d7ef404611cd
hikobend <hiko.bkend@gmail.com>
2023-08-09 09:11:41 +0900

    database_password作成

--- modules/parameter_store/main.tf
+++ modules/parameter_store/main.tf
@@ -1,7 +1,16 @@
-module "secret" {
+module "database_user" {
   source = "terraform-aws-modules/ssm-parameter/aws"
 
   name                 = "/${var.env}/database_user"
+  value                = "change me"
+  ignore_value_changes = true
+  secure_type          = true
+}
+
+module "database_password" {
+  source = "terraform-aws-modules/ssm-parameter/aws"
+
+  name                 = "/${var.env}/database_password"
   value                = "change me"
   ignore_value_changes = true
   secure_type          = true
