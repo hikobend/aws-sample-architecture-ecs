0f3e45bec1a45ffc102446aa14fb381e4208e5f8
hikobend <hiko.bkend@gmail.com>
2023-08-09 09:43:59 +0900

    parameter_storeを先に作成

--- envs/dev/datas.tf
+++ envs/dev/datas.tf
@@ -1,7 +1,7 @@
-data "aws_ssm_parameter" "database_user" {
-  name = "/dev/database_user"
-}
+# data "aws_ssm_parameter" "database_user" {
+#   name = "/dev/database_user"
+# }
 
-data "aws_ssm_parameter" "database_password" {
-  name = "/dev/database_password"
-}
+# data "aws_ssm_parameter" "database_password" {
+#   name = "/dev/database_password"
+# }
