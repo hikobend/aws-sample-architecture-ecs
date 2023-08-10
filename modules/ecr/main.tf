01484d0db27edebdf430b848846fb3508e25fae8
hikobend <hiko.bkend@gmail.com>
2023-08-10 23:22:06 +0900

    backendを変更

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -11,7 +11,32 @@ module "frontend-ecr" {
         description  = "Keep last 5 images",
         selection = {
           tagStatus     = "any",
-          tagPrefixList = ["${var.env}-"],
+          tagPrefixList = ["${var.env}-frontend-"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 5
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
+module "backend-ecr" {
+  source  = "terraform-aws-modules/ecr/aws"
+  version = "1.6.0"
+
+  repository_name = "${var.env}-backend-ecr"
+  # repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 5 images",
+        selection = {
+          tagStatus     = "any",
+          tagPrefixList = ["${var.env}-backend-"],
           countType     = "imageCountMoreThan",
           countNumber   = 5
         },
